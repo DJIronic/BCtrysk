006bf68c0e3cb67a90c147e3c6f13e7b381211a8
Šimon Čecháček <dj@life-games.cz>
2021-01-05 17:32:31 +0100

    Update LoginEncryptionUtils.java
    Translation fixed for BC clients utils

--- connector/src/main/java/org/geysermc/connector/utils/LoginEncryptionUtils.java
+++ connector/src/main/java/org/geysermc/connector/utils/LoginEncryptionUtils.java
@@ -172,8 +172,8 @@ public class LoginEncryptionUtils {
         String userLanguage = session.getLocale();
         CustomFormWindow window = new CustomFormBuilder(LanguageUtils.getPlayerLocaleString("geyser.auth.login.form.details.title", userLanguage))
                 .addComponent(new LabelComponent(LanguageUtils.getPlayerLocaleString("geyser.auth.login.form.details.desc", userLanguage)))
-                .addComponent(new InputComponent(LanguageUtils.getPlayerLocaleString("geyser.auth.login.form.details.email", userLanguage), "account@geysermc.org", ""))
-                .addComponent(new InputComponent(LanguageUtils.getPlayerLocaleString("geyser.auth.login.form.details.pass", userLanguage), "123456", ""))
+                .addComponent(new InputComponent(LanguageUtils.getPlayerLocaleString("geyser.auth.login.form.details.email", userLanguage), "mcucet@burningcube.eu", ""))
+                .addComponent(new InputComponent(LanguageUtils.getPlayerLocaleString("geyser.auth.login.form.details.pass", userLanguage), "heslo123", ""))
                 .build();
 
         session.sendForm(window, AUTH_DETAILS_FORM_ID);
